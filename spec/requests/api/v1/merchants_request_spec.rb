9f2a025b16063cab496444e19477f5a1a7582f75
Vince Carollo <vincecarollo@gmail.com>
2019-06-26 19:16:14 -0600

    Start spec for merchants business intel requests

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,6 +2,42 @@ require 'rails_helper'
 
 describe 'Merchants API' do
 
+  before :each do
+    @merchant_1 = create(:merchant, name: "Merch1")
+    @merchant_2 = create(:merchant, name: "Merch2")
+    @merchant_3 = create(:merchant, name: "Merch3")
+    @merchant_4 = create(:merchant, name: "Merch4")
+    @merchant_5 = create(:merchant, name: "Merch5")
+    @merchant_6 = create(:merchant, name: "Merch6")
+    @item_1 = create(:item, merchant: @merchant_1)
+    @item_2 = create(:item, merchant: @merchant_2)
+    @item_3 = create(:item, merchant: @merchant_3)
+    @item_4 = create(:item, merchant: @merchant_4)
+    @item_5 = create(:item, merchant: @merchant_5)
+    @item_6 = create(:item, merchant: @merchant_6)
+    @invoice_1 = create(:invoice, merchant: @merchant_1)
+    @invoice_2 = create(:invoice, merchant: @merchant_2)
+    @invoice_3 = create(:invoice, merchant: @merchant_3)
+    @invoice_4 = create(:invoice, merchant: @merchant_4)
+    @invoice_5 = create(:invoice, merchant: @merchant_5)
+    @invoice_6 = create(:invoice, merchant: @merchant_6)
+    @invoice_7 = create(:invoice, merchant: @merchant_6)
+    @transaction_1 = create(:transaction, invoice: @invoice_1)
+    @transaction_2 = create(:transaction, invoice: @invoice_2)
+    @transaction_3 = create(:transaction, invoice: @invoice_3)
+    @transaction_4 = create(:transaction, invoice: @invoice_4)
+    @transaction_5 = create(:transaction, invoice: @invoice_5)
+    @transaction_6 = create(:transaction, invoice: @invoice_6)
+    @transaction_7 = create(:transaction, invoice: @invoice_7, result: 'failed')
+    @ii_1 = InvoiceItem.create(quantity: 1, item: @item_1, unit_price: 1.0, invoice: @invoice_1)
+    @ii_2 = InvoiceItem.create(quantity: 2, item: @item_2, unit_price: 2.0, invoice: @invoice_2)
+    @ii_3 = InvoiceItem.create(quantity: 3, item: @item_3, unit_price: 3.0, invoice: @invoice_3)
+    @ii_4 = InvoiceItem.create(quantity: 4, item: @item_4, unit_price: 4.0, invoice: @invoice_4)
+    @ii_5 = InvoiceItem.create(quantity: 5, item: @item_5, unit_price: 5.0, invoice: @invoice_5)
+    @ii_6 = InvoiceItem.create(quantity: 6, item: @item_6, unit_price: 6.0, invoice: @invoice_6)
+    @ii_7 = InvoiceItem.create(quantity: 7, item: @item_6, unit_price: 7.0, invoice: @invoice_7)
+  end
+
   it "sends a list of merchants" do
     create_list(:merchant, 3)
 
@@ -11,7 +47,7 @@ describe 'Merchants API' do
 
     merchants = JSON.parse(response.body)
 
-    expect(merchants['data'].count).to eq(3)
+    expect(merchants['data'].count).to eq(9)
   end
 
   it "sends a merchant by id" do
@@ -127,7 +163,8 @@ describe 'Merchants API' do
 
     merchant = JSON.parse(response.body)
 
-    expect(ids).to include(merchant['data']['id'].to_i)
+    id = merchant['data']['id']
+    expect(Merchant.find(id)).to be_instance_of(Merchant)
   end
 
   it "return items associated with a merchant" do
@@ -161,6 +198,37 @@ describe 'Merchants API' do
     invoices = JSON.parse(response.body)
 
     expect(invoices['data'].count).to eq(2)
+  end
+
+  it "returns top merchants by revenue" do
+    get "/api/v1/merchants/most_revenue?quantity=5"
+
+    expect(response).to be_successful
+
+    top_merchants = JSON.parse(response.body)
+
+    expect(top_merchants['data'].length).to eq(5)
+    expect(top_merchants['data'].first['attributes']['id']).to eq(@merchant_6.id)
+    expect(top_merchants['data'].last['attributes']['id']).to eq(@merchant_2.id)
+  end
+
+  it "returns top merchants by items sold" do
+    get "/api/v1/merchants/most_items?quantity=5"
+
+    expect(response).to be_successful
+
+    top_merchants = JSON.parse(response.body)
+    expect(top_merchants['data'].length).to eq(5)
+    expect(top_merchants['data'].first['attributes']['id']).to eq(@merchant_6.id)
+    expect(top_merchants['data'].last['attributes']['id']).to eq(@merchant_2.id)
+  end
+
+  xit "returns total revenue for given date" do
+    get "/api/v1/merchants/revenue?date=2012-03-16"
+
+    expect(response).to be_successful
+
+    total_revenue = JSON.parse(response.body)
   end
 
 end
