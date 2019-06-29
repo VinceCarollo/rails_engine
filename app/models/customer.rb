21b0c73fca13ade4b951573d3e4812ebd9fd68e0
Vince Carollo <vincecarollo@gmail.com>
2019-06-29 12:53:35 -0600

    Add class method top_bought_from merchant to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,14 @@ class Customer < ApplicationRecord
   def transactions
     Transaction.where(invoice: self.invoices).order(:id)
   end
+
+  def self.top_bought_from(merchant)
+    joins(invoices: [:transactions])
+        .merge(Transaction.successful)
+        .select('customers.*, COUNT(transactions.id) AS total_transactions')
+        .where("invoices.merchant_id=#{merchant.id}")
+        .group(:id)
+        .order('total_transactions DESC')
+        .first
+  end
 end
