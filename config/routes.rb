d38a87120fff707fd1b4c0628bccc7c8ed4e408d
Vince Carollo <vincecarollo@gmail.com>
2019-06-29 16:14:55 -0600

    Add routes for items bi endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -20,10 +20,13 @@ Rails.application.routes.draw do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
         get '/random', to: 'search#show'
+        get '/most_revenue', to: 'top_revenue_items#index'
+        get '/most_items', to: 'top_sold_items#index'
       end
       resources :items, only: [:index, :show] do
         get "/invoice_items", to: 'items/invoice_items#index'
         get "/merchant", to: 'items/merchants#show'
+        get '/best_day', to: 'items/days#show'
       end
       namespace :invoices do
         get '/find', to: 'search#show'
